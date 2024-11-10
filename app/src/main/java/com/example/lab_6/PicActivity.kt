aaddcfaee39d5f926a3c9eb16f101af55599997e
OldM1lk <MrlaimWin@gmail.com>
2024-11-10 10:48:24 +0700

    Add new branch and code for task1

--- app/src/main/java/com/example/lab_6/PicActivity.kt
+++ app/src/main/java/com/example/lab_6/PicActivity.kt
@@ -1,30 +1,29 @@
 package com.example.lab_6
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
 
-class MainActivity : AppCompatActivity() {
+class PicActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.pic_layout)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val btnShowPic: Button = findViewById(R.id.btn_show_pic)
+        title = "Картинка"
 
-        btnShowPic.setOnClickListener {
-            val intent = Intent(this, PicActivity::class.java)
-            intent.putExtra("picLink", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXMKlTGvFF15XZA7pWHbMlh4Tv8rMdVqdmig&s")
-            startActivity(intent)
-        }
+        val picView: ImageView = findViewById(R.id.picView)
+        val picLink = intent.getStringExtra("picLink")
+
+        Glide.with(this).load(picLink).into(picView)
     }
 }
